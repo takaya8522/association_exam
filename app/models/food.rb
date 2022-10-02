b7b4dd03b476a0ce5d6e6bb35986f671b250442d
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-10-02 19:48:12 +0900

    修正3

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,4 @@
 class Food < ApplicationRecord
-  has_many :order_foods
+  has_many :orders, through: :order_foods
   belongs_to :shop
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :customer
   belongs_to :address
-  has_many :order_foods
+  has_many :foods, through: :order_foods
 end
