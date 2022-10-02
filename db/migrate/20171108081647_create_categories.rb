1c430a6fcc6a04f6bdbf81086b83c25a16d5d92c
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-10-02 21:11:41 +0900

    修正9

--- db/migrate/20171108081647_create_categories.rb
+++ db/migrate/20171108081647_create_categories.rb
@@ -2,7 +2,9 @@ class CreateCategories < ActiveRecord::Migration[5.1]
   def change
     create_table :categories do |t|
       t.string :name
-
+      t.references :italian_food, foreign_key: true
+      t.references :japanese_food, foreign_key: true
+      t.references :chinese_food, foreign_key: true
       t.timestamps
     end
   end
